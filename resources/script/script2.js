7cfccb794a97a2cf6bb65fb06adcf51dc439f093
Ayomide Popoola <popoolaayomide37@gmail.com>
2021-03-20 21:49:15 -0700

    [modified the background]

--- resources/script/script2.js
+++ resources/script/script2.js
@@ -1,14 +1,14 @@
 let questions = [
     {question:"what is the 20% of 400", option1: 50, option2: 70, option3: 80, option4: "All of the above", answer:"80", selected:''},
     {question:"solve this equation: 2x + 24 -5x = 3", option1: 12, option2: 24, option3: 7, option4: "7x", answer:7},
-    {question:"2x+x-1, find x", option1: "3x+1", option2: "3x-1", option3: "2x", option4: "none of the above", answer:"3x+1"},
-    {question:"what is the formular of a diameter", option1: "2R", option2: "R+R", option3: "d", option4: "r * R", answer:"2R"},
-    {question:"which is not a polygon", option1: "circle", option2: "Rhombus", option3: "Square", option4: "hexagon", answer:"circle"},
-    {question:"what is the sum of angle of a octagon", option1: 720, option2: 1080, option3: 860, option4: "none of the above", answer:"1080"},
-    {question:"divide 10805 by 5", option1: 261, option2: 84, option3: 205, option4: 2161, answer:2161},
-    {question:"A man bought 10 cars, he gave two to his brother and gave two also to his company, how many does he have if the police had seize four of his cars", option1: 4, option2: 2, option3: 6, option4: "i don't know", answer:6},
-    {question: "what is the shape of a human face", option1: "oval", option2: "sphere", option3: "no shape", option4: "square", answer:"oval"},
-    {question:"solve 2(x-1)(4x-2)", option1: "2-2x", option2: "2(1-x)", option3: "2x", option4: "4x", answer:"2(1-x)"}
+    // {question:"2x+x-1, find x", option1: "3x+1", option2: "3x-1", option3: "2x", option4: "none of the above", answer:"3x+1"},
+    // {question:"what is the formular of a diameter", option1: "2R", option2: "R+R", option3: "d", option4: "r * R", answer:"2R"},
+    // {question:"which is not a polygon", option1: "circle", option2: "Rhombus", option3: "Square", option4: "hexagon", answer:"circle"},
+    // {question:"what is the sum of angle of a octagon", option1: 720, option2: 1080, option3: 860, option4: "none of the above", answer:"1080"},
+    // {question:"divide 10805 by 5", option1: 261, option2: 84, option3: 205, option4: 2161, answer:2161},
+    // {question:"A man bought 10 cars, he gave two to his brother and gave two also to his company, how many does he have if the police had seize four of his cars", option1: 4, option2: 2, option3: 6, option4: "i don't know", answer:6},
+    // {question: "what is the shape of a human face", option1: "oval", option2: "sphere", option3: "no shape", option4: "square", answer:"oval"},
+    // {question:"solve 2(x-1)(4x-2)", option1: "2-2x", option2: "2(1-x)", option3: "2x", option4: "4x", answer:"2(1-x)"}
 
 ]
 let wholeQuestionContainer = document.getElementById("wholeContainer")
@@ -27,7 +27,7 @@ let score = 0
 let count = 300
 
 let interval = setInterval(function(){
-    document.getElementById("count").innerHTML = `seconds: ${count} secs` ;
+    document.getElementById("count").innerHTML = `${count} seconds` ;
     count--;
     if(count === 0){
         clearInterval(interval);
@@ -68,8 +68,8 @@ nextBtn.addEventListener("click", function(){
         }if(questionposit == questions.length-1){
             submitBtn.style.display = "block"
             submitBtn.style.position = "relative"
-            submitBtn.style.bottom = '50px'
-            submitPara.style.display = "block"
+            submitBtn.style.bottom = '40px'
+            submitPara.style.display = "bsubmitBtn.style.bottom = '40px'lock"
             nextBtn.style.display = "none"
             prevBtn.style.display = "block"
         }
@@ -86,7 +86,8 @@ prevBtn.addEventListener("click", function(){
     submitPara.style.display = "none"
     nextBtn.style.display = "block" 
     nextBtn.style.position = "relative"
-    nextBtn.style.bottom = '50px'
+    nextBtn.style.bottom = '5px'
+  
         questionposit--
         loadQuestions(questionposit) 
             
